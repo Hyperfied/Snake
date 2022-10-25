6cd11bed1dd5a0e5a066a2b09eb0d3b99dd05ca4
Hyperfied <hyperfied@outlook.com>
2022-10-25 12:19:23 +0100

    Create VectorChar2D class

--- Snake++/Main.cpp
+++ Snake++/Main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <vector>
+#include "VectorChar2D.h"
+
+int boardWidth = 10;
+int boardHeight = 10;
+
+char boardChar = '-';
+char playerChar = 'O';
+char fruitChar = '#';
+
+VectorChar2D initBoard()
+{
+	VectorChar2D board;
+
+	for (int i = 0; i < boardHeight; i++)
+	{
+		std::vector<char> addVector;
+		board.push_back(addVector);
+		for (int j = 0; j < boardWidth; j++)
+		{
+			board[i].push_back(boardChar);
+		}
+	}
+
+	return board;
+}
+
+void displayBoard(VectorChar2D board)
+{
+	for (size_t i = 0; i < board.size(); i++)
+	{
+		for (size_t j = 0; j < board[i].size(); j++)
+		{
+			std::cout << board[i][j];
+		}
+		std::cout << std::endl;
+	}
+}
+
+
 
 int main()
 {
-	std::cout << "Hello World!";
+	VectorChar2D board = initBoard();
+	displayBoard(board);
 }
---
+++ Snake++/VectorChar2D.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+
+class VectorChar2D : public std::vector<std::vector<char>>
+{
+	 
+};
